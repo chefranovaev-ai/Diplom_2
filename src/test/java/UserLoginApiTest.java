8b25fd436dc11cb5b74806d6db0ab34235543887
Elena Chefranova <chefranovaev@yandex.ru>
2026-05-31 12:31:09 +0400

    разбила на два негативных теста логирование пользователя

--- src/test/java/UserLoginApiTest.java
+++ src/test/java/UserLoginApiTest.java
@@ -14,9 +14,8 @@ public class UserLoginApiTest extends BaseApiTest {
     @Before
     @Override
     public void setUp() {
-        super.setUp(); // Вызываем базовый URL и фильтры из BaseApiTest
+        super.setUp();
 
-        // Переносим создание пользователя сюда, так как он нужен для тестов логина
         user = userSteps.generateUserData();
         Response registerResponse = userSteps.registerUser(user);
         accessToken = userSteps.getAccessToken(registerResponse);
@@ -29,15 +28,22 @@ public class UserLoginApiTest extends BaseApiTest {
         Response loginResponse = userSteps.loginUser(user.getEmail(), user.getPassword());
 
         userSteps.checkSuccessResponse(loginResponse);
-        // Обновляем токен, если сервер сгенерировал новый при логине
         accessToken = userSteps.getAccessToken(loginResponse);
     }
 
     @Test
-    @DisplayName("Вход с неверным логином и паролем — Ошибка")
-    @Description("Проверяет попытку авторизации с некорректным логином или паролем. Ожидается код 401 Unauthorized и сообщение об ошибке.")
-    public void testLoginWithInvalidCredentialsFails() {
-        Response response = userSteps.loginUser("non_existent_burger_user_999@mail.com", "wrong_password");
+    @DisplayName("Вход с неверным логином — Ошибка")
+    @Description("Проверяет попытку авторизации с некорректным логином. Ожидается код 401 Unauthorized и сообщение об ошибке.")
+    public void testLoginWithInvalidLoginFails() {
+        Response response = userSteps.loginUser("non_existent_burger_user_999@mail.com", user.getPassword());
+        userSteps.checkUnauthorizedResponse(response, "email or password are incorrect");
+    }
+    @Test
+    @DisplayName("Вход с неверным паролем — Ошибка")
+    @Description("Проверяет попытку авторизации существующего пользователя с некорректным паролем. Ожидается код 401 Unauthorized и сообщение об ошибке.")
+    public void testLoginWithInvalidPasswordFails() {
+
+        Response response = userSteps.loginUser(user.getEmail(), "completely_wrong_password_123");
         userSteps.checkUnauthorizedResponse(response, "email or password are incorrect");
     }
 }
